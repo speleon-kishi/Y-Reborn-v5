e1b0732bbc6453f614c07eac0a71b779f44b9fe6
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-01-28 01:33:24 -0600

    Update DownloadsVideoController.h

--- Controllers/DownloadsVideoController.h
+++ Controllers/DownloadsVideoController.h
@@ -4,6 +4,8 @@
 
 @interface DownloadsVideoController : UIViewController <UICollectionViewDelegate, UICollectionViewDataSource, UISearchBarDelegate, UIScrollViewDelegate, AVPlayerViewControllerDelegate>
 @property (nonatomic, strong) UICollectionView *collectionView;
+@property (nonatomic, strong) UILabel *textLabel;
+@property (nonatomic, strong) UILabel *detailTextLabel;
 @property (nonatomic, strong) UISearchBar *searchBar;
 @property (nonatomic, strong) NSArray *allItems;
 @property (nonatomic, strong) NSArray *filteredItems;
