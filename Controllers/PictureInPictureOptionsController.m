893483d8c43574ffeeadda734ff3b9b8bb469a07
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-06-16 15:08:20 -0500

    Refactored PictureInPictureOptionsController.m

--- Controllers/PictureInPictureOptionsController.m
+++ Controllers/PictureInPictureOptionsController.m
@@ -1,6 +1,22 @@
 #import "PictureInPictureOptionsController.h"
 #import "Localization.h"
 
+#define BOOL_FOR_KEY(KEY) [[NSUserDefaults standardUserDefaults] boolForKey:KEY]
+#define SET_BOOL_FOR_KEY(KEY, VALUE) [[NSUserDefaults standardUserDefaults] setBool:VALUE forKey:KEY]; [[NSUserDefaults standardUserDefaults] synchronize];
+
+#define TOGGLE_SETTING(KEY, SENDER) \
+if ([SENDER isOn]) { \
+    SET_BOOL_FOR_KEY(KEY, YES); \
+} else { \
+    SET_BOOL_FOR_KEY(KEY, NO); \
+}
+
+#define CREATE_SWITCH(NAME, SELECTOR, KEY) \
+UISwitch *NAME = [[UISwitch alloc] initWithFrame:CGRectZero]; \
+[NAME addTarget:self action:@selector(SELECTOR:) forControlEvents:UIControlEventValueChanged]; \
+NAME.on = BOOL_FOR_KEY(KEY);\
+cell.accessoryView = NAME;
+
 @interface PictureInPictureOptionsController ()
 - (void)coloursView;
 @end
@@ -28,7 +44,7 @@
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
     [self.view addSubview:self.tableView];
-    
+
     [NSLayoutConstraint activateConstraints:@[
         [self.tableView.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
         [self.tableView.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor],
@@ -46,8 +62,7 @@
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    static NSString *CellIdentifier = @"PictureInPictureTableViewCell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PictureInPictureTableViewCell"];
 
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:nil];
@@ -61,8 +76,6 @@
         else {
             cell.backgroundColor = [UIColor colorWithRed:0.110 green:0.110 blue:0.118 alpha:1.0];
             cell.textLabel.textColor = [UIColor whiteColor];
-	    cell.textLabel.shadowColor = [UIColor blackColor];
-            cell.textLabel.shadowOffset = CGSizeMake(1.0, 1.0);
             cell.detailTextLabel.textColor = [UIColor whiteColor];
         }
         cell.selectionStyle = UITableViewCellSelectionStyleNone;
@@ -71,25 +84,16 @@
             if ([[NSUserDefaults standardUserDefaults] boolForKey:@"kRebornIHaveYouTubePremium"] == YES) {
                 cell.accessoryType = UITableViewCellAccessoryDetailButton;
             } else {
-                UISwitch *enablePictureInPicture = [[UISwitch alloc] initWithFrame:CGRectZero];
-                [enablePictureInPicture addTarget:self action:@selector(toggleEnablePictureInPicture:) forControlEvents:UIControlEventValueChanged];
-                enablePictureInPicture.on = [[NSUserDefaults standardUserDefaults] boolForKey:@"kEnablePictureInPictureVTwo"];
-                cell.accessoryView = enablePictureInPicture;
+                CREATE_SWITCH(enablePictureInPicture, toggleEnablePictureInPicture, @"kEnablePictureInPictureVTwo");
             }
         }
         if (indexPath.row == 1) {
             cell.textLabel.text = LOC(@"HIDE_PIP_ADS_BADGE");
-            UISwitch *hidePictureInPictureAdsBadge = [[UISwitch alloc] initWithFrame:CGRectZero];
-            [hidePictureInPictureAdsBadge addTarget:self action:@selector(toggleHidePictureInPictureAdsBadge:) forControlEvents:UIControlEventValueChanged];
-            hidePictureInPictureAdsBadge.on = [[NSUserDefaults standardUserDefaults] boolForKey:@"kHidePictureInPictureAdsBadge"];
-            cell.accessoryView = hidePictureInPictureAdsBadge;
+            CREATE_SWITCH(hidePictureInPictureAdsBadge, toggleHidePictureInPictureAdsBadge, @"kHidePictureInPictureAdsBadge");
         }
         if (indexPath.row == 2) {
             cell.textLabel.text = LOC(@"HIDE_PIP_SPONSOR_BADGE");
-            UISwitch *hidePictureInPictureSponsorBadge = [[UISwitch alloc] initWithFrame:CGRectZero];
-            [hidePictureInPictureSponsorBadge addTarget:self action:@selector(toggleHidePictureInPictureSponsorBadge:) forControlEvents:UIControlEventValueChanged];
-            hidePictureInPictureSponsorBadge.on = [[NSUserDefaults standardUserDefaults] boolForKey:@"kHidePictureInPictureSponsorBadge"];
-            cell.accessoryView = hidePictureInPictureSponsorBadge;
+            CREATE_SWITCH(hidePictureInPictureSponsorBadge, toggleHidePictureInPictureSponsorBadge, @"kHidePictureInPictureSponsorBadge");
         }
     }
     return cell;
@@ -125,12 +129,6 @@
     [self.tableView reloadData];
 }
 
-- (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-    self.view.layer.cornerRadius = 10.0;
-    self.view.layer.masksToBounds = YES;
-}
-
 @end
 
 @implementation PictureInPictureOptionsController (Privates)
@@ -140,33 +138,15 @@
 }
 
 - (void)toggleEnablePictureInPicture:(UISwitch *)sender {
-    if ([sender isOn]) {
-        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"kEnablePictureInPictureVTwo"];
-        [[NSUserDefaults standardUserDefaults] synchronize];
-    } else {
-        [[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"kEnablePictureInPictureVTwo"];
-        [[NSUserDefaults standardUserDefaults] synchronize];
-    }
+    TOGGLE_SETTING(@"kEnablePictureInPictureVTwo", sender);
 }
 
 - (void)toggleHidePictureInPictureAdsBadge:(UISwitch *)sender {
-    if ([sender isOn]) {
-        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"kHidePictureInPictureAdsBadge"];
-        [[NSUserDefaults standardUserDefaults] synchronize];
-    } else {
-        [[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"kHidePictureInPictureAdsBadge"];
-        [[NSUserDefaults standardUserDefaults] synchronize];
-    }
+    TOGGLE_SETTING(@"kHidePictureInPictureAdsBadge", sender);
 }
 
 - (void)toggleHidePictureInPictureSponsorBadge:(UISwitch *)sender {
-    if ([sender isOn]) {
-        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"kHidePictureInPictureSponsorBadge"];
-        [[NSUserDefaults standardUserDefaults] synchronize];
-    } else {
-        [[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"kHidePictureInPictureSponsorBadge"];
-        [[NSUserDefaults standardUserDefaults] synchronize];
-    }
+    TOGGLE_SETTING(@"kHidePictureInPictureSponsorBadge", sender);
 }
 
 @end
