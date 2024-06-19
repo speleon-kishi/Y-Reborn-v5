c8db9fc2266ede3e5b43516f4f8f91098bbf2b03
arichornlover <78001398+arichornlover@users.noreply.github.com>
2024-06-19 02:31:50 -0500

    Reverse MobileFFmpeg stuff

--- MobileFFmpeg/MediaInformation.h
+++ MobileFFmpeg/MediaInformation.h
@@ -1,171 +1,80 @@
 /*
- * Copyright (c) 2018-2022 Taner Sener
+ * Copyright (c) 2018, 2020 Taner Sener
  *
- * This file is part of FFmpegKit.
+ * This file is part of MobileFFmpeg.
  *
- * FFmpegKit is free software: you can redistribute it and/or modify
+ * MobileFFmpeg is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * FFmpegKit is distributed in the hope that it will be useful,
+ * MobileFFmpeg is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with FFmpegKit.  If not, see <http://www.gnu.org/licenses/>.
+ * along with MobileFFmpeg.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef FFMPEG_KIT_STREAM_INFORMATION_H
-#define FFMPEG_KIT_STREAM_INFORMATION_H
-
-#import <Foundation/Foundation.h>
-
-extern NSString* const StreamKeyIndex;
-extern NSString* const StreamKeyType;
-extern NSString* const StreamKeyCodec;
-extern NSString* const StreamKeyCodecLong;
-extern NSString* const StreamKeyFormat;
-extern NSString* const StreamKeyWidth;
-extern NSString* const StreamKeyHeight;
-extern NSString* const StreamKeyBitRate;
-extern NSString* const StreamKeySampleRate;
-extern NSString* const StreamKeySampleFormat;
-extern NSString* const StreamKeyChannelLayout;
-extern NSString* const StreamKeySampleAspectRatio;
-extern NSString* const StreamKeyDisplayAspectRatio;
-extern NSString* const StreamKeyAverageFrameRate;
-extern NSString* const StreamKeyRealFrameRate;
-extern NSString* const StreamKeyTimeBase;
-extern NSString* const StreamKeyCodecTimeBase;
-extern NSString* const StreamKeyTags;
+#include <Foundation/Foundation.h>
+#include "StreamInformation.h"
 
 /**
- * Stream information class.
+ * Media information class.
  */
-@interface StreamInformation : NSObject
+@interface MediaInformation : NSObject
 
-- (instancetype)init:(NSDictionary*)streamDictionary;
+- (instancetype)init:(NSDictionary*)mediaDictionary withStreams:(NSArray*)streams;
 
 /**
- * Returns stream index.
+ * Returns file name.
  *
- * @return stream index, starting from zero
+ * @return media file name
  */
-- (NSNumber*)getIndex;
+- (NSString*)getFilename;
 
 /**
- * Returns stream type.
+ * Returns format.
  *
- * @return stream type; audio or video
- */
-- (NSString*)getType;
-
-/**
- * Returns stream codec.
- *
- * @return stream codec
- */
-- (NSString*)getCodec;
-
-/**
- * Returns stream codec in long format.
- *
- * @return stream codec with additional profile and mode information
- */
-- (NSString*)getCodecLong;
-
-/**
- * Returns stream format.
- *
- * @return stream format
+ * @return media format
  */
 - (NSString*)getFormat;
 
 /**
- * Returns width.
+ * Returns long format.
  *
- * @return width in pixels
+ * @return media long format
  */
-- (NSNumber*)getWidth;
+- (NSString*)getLongFormat;
 
 /**
- * Returns height.
+ * Returns duration.
  *
- * @return height in pixels
+ * @return media duration in milliseconds
  */
-- (NSNumber*)getHeight;
+- (NSString*)getDuration;
+
+/**
+ * Returns start time.
+ *
+ * @return media start time in milliseconds
+ */
+- (NSString*)getStartTime;
+
+/**
+ * Returns size.
+ *
+ * @return media size in bytes
+ */
+- (NSString*)getSize;
 
 /**
  * Returns bitrate.
  *
- * @return bitrate in kb/s
+ * @return media bitrate in kb/s
  */
 - (NSString*)getBitrate;
-
-/**
- * Returns sample rate.
- *
- * @return sample rate in hz
- */
-- (NSString*)getSampleRate;
-
-/**
- * Returns sample format.
- *
- * @return sample format
- */
-- (NSString*)getSampleFormat;
-
-/**
- * Returns channel layout.
- *
- * @return channel layout
- */
-- (NSString*)getChannelLayout;
-
-/**
- * Returns sample aspect ratio.
- *
- * @return sample aspect ratio
- */
-- (NSString*)getSampleAspectRatio;
-
-/**
- * Returns display aspect ratio.
- *
- * @return display aspect ratio
- */
-- (NSString*)getDisplayAspectRatio;
-
-/**
- * Returns average frame rate.
- *
- * @return average frame rate in fps
- */
-- (NSString*)getAverageFrameRate;
-
-/**
- * Returns real frame rate.
- *
- * @return real frame rate in tbr
- */
-- (NSString*)getRealFrameRate;
-
-/**
- * Returns time base.
- *
- * @return time base in tbn
- */
-- (NSString*)getTimeBase;
-
-/**
- * Returns codec time base.
- *
- * @return codec time base in tbc
- */
-- (NSString*)getCodecTimeBase;
 
 /**
  * Returns all tags.
@@ -175,33 +84,45 @@ extern NSString* const StreamKeyTags;
 - (NSDictionary*)getTags;
 
 /**
- * Returns the stream property associated with the key.
+ * Returns all streams.
  *
- * @return stream property as string or nil if the key is not found
+ * @return streams array
+ */
+- (NSArray*)getStreams;
+
+/**
+ * Returns the media property associated with the key.
+ *
+ * @return media property as string or nil if the key is not found
  */
 - (NSString*)getStringProperty:(NSString*)key;
 
 /**
- * Returns the stream property associated with the key.
+ * Returns the media property associated with the key.
  *
- * @return stream property as number or nil if the key is not found
+ * @return media property as number or nil if the key is not found
  */
 - (NSNumber*)getNumberProperty:(NSString*)key;
 
 /**
- * Returns the stream property associated with the key.
+ * Returns the media properties associated with the key.
  *
- * @return stream property as id or nil if the key is not found
+ * @return media properties in a dictionary or nil if the key is not found
 */
-- (id)getProperty:(NSString*)key;
+- (NSDictionary*)getProperties:(NSString*)key;
 
 /**
- * Returns all stream properties defined.
+ * Returns all media properties.
  *
- * @return all stream properties in a dictionary or nil if no properties are defined
+ * @return all media properties in a dictionary or nil if no media properties are defined
+*/
+- (NSDictionary*)getMediaProperties;
+
+/**
+ * Returns all properties defined.
+ *
+ * @return all properties in a dictionary or nil if no properties are defined
 */
 - (NSDictionary*)getAllProperties;
 
 @end
-
-#endif // FFMPEG_KIT_STREAM_INFORMATION_H
