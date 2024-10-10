e1833bd09864384c0820d5bff0edc2a81edce8b7
aric3435 (INACTIVE) <78001398+arichornlover@users.noreply.github.com>
2024-10-10 14:17:57 -0500

    Redesign CreditsController.m

--- Controllers/CreditsController.m
+++ Controllers/CreditsController.m
@@ -1,162 +1,75 @@
-#import "CreditsController.h"
+#import "CreditsViewController.h"
 
-@interface CreditsController ()
-- (void)coloursView;
+@interface CreditsViewController ()
+
+@property (strong, nonatomic) NSArray *credits;
+@property (strong, nonatomic) NSArray *roles;
+@property (strong, nonatomic) NSArray *urls;
+
 @end
 
-@implementation CreditsController
+@implementation CreditsViewController
 
-- (void)loadView {
-	[super loadView];
-    [self coloursView];
-
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
     self.title = @"Credits";
-
-    UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(done)];
-    self.navigationItem.rightBarButtonItem = doneButton;
-
-    if (@available(iOS 15.0, *)) {
-    	[self.tableView setSectionHeaderTopPadding:0.0f];
-	}
+    [self.navigationController.navigationBar setTitleTextAttributes:@{NSFontAttributeName: [UIFont fontWithName:@"YTSans-Bold" size:22], NSForegroundColorAttributeName: [UIColor whiteColor]}];
+    self.tableView.backgroundColor = [UIColor systemBackgroundColor];
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+    self.tableView.separatorColor = [UIColor lightGrayColor];
+    
+    self.credits = @[
+        @{@"name": @"Lillie", @"role": @"Original Developer", @"url": @""},
+        @{@"name": @"Alpha_Stream", @"role": @"Icon Designer", @"url": @"https://twitter.com/Kutarin_"},
+        @{@"name": @"kirb", @"role": @"Development Support", @"url": @"https://github.com/kirb"},
+        @{@"name": @"Dayanch96", @"role": @"Features: \"Red Progress Bar\", \"Gray Buffer Progress\" \"Stick Navigation Bar\", \"Disable Double tap to skip\"", @"url": @"https://github.com/Dayanch96"},
+        @{@"name": @"PoomSmart", @"role": @"Features: \"YouTube-X/Adblock\", \"AutoPlay In Fullscreen\"", @"url": @"https://twitter.com/PoomSmart"},
+        @{@"name": @"Snoolie", @"role": @"Features: \"Enable Extra Video Speed\"", @"url": @"https://github.com/0xilis"}
+        @{@"name": @"jkhsjdhjs", @"role": @"Features: \"YouTube Native Share\"", @"url": @"https://github.com/jkhsjdhjs"}
+    ];
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 2;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    if (section == 0) {
-        return 1;
-    }
-    if (section == 1) {
-        return 4;
-    }
-    return 0;
+    return self.credits.count;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return UITableViewAutomaticDimension;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView estimatedHeightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 60;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    static NSString *CellIdentifier = @"CreditsTableViewCell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-
-    if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:nil];
-        cell.textLabel.adjustsFontSizeToFitWidth = YES;
-        cell.detailTextLabel.adjustsFontSizeToFitWidth = YES;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CreditCell"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"CreditCell"];
+        cell.textLabel.numberOfLines = 0;
+        cell.detailTextLabel.numberOfLines = 0;
         cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-        if (self.traitCollection.userInterfaceStyle == UIUserInterfaceStyleLight) {
-            cell.backgroundColor = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
-            cell.textLabel.textColor = [UIColor blackColor];
-            cell.detailTextLabel.textColor = [UIColor blackColor];
-        }
-        else {
-            cell.backgroundColor = [UIColor colorWithRed:0.110 green:0.110 blue:0.118 alpha:1.0];
-            cell.textLabel.textColor = [UIColor whiteColor];
-            cell.textLabel.shadowColor = [UIColor blackColor];
-            cell.textLabel.shadowOffset = CGSizeMake(1.0, 1.0);
-            cell.detailTextLabel.textColor = [UIColor whiteColor];
-        }
-        if (indexPath.section == 0) {
-            if (indexPath.row == 0) {
-                cell.textLabel.text = @"Lillie";
-                cell.detailTextLabel.text = @"Developer";
-            }
-        }
-        if (indexPath.section == 1) {
-            if (indexPath.row == 0) {
-                cell.textLabel.text = @"Alpha_Stream";
-                cell.detailTextLabel.text = @"Icon Designer";
-            }
-            if (indexPath.row == 1) {
-                cell.textLabel.text = @"kirb";
-                cell.detailTextLabel.text = @"Development Support";
-            }
-            if (indexPath.row == 2) {
-                cell.textLabel.text = @"Dayanch96";
-                cell.detailTextLabel.text = @"Features: \"Red Progress Bar\", \"Gray Buffer Progress\" \"Stick Navigation Bar\", \"Disable Double tap to skip\"";
-            }
-            if (indexPath.row == 3) {
-                cell.textLabel.text = @"PoomSmart";
-                cell.detailTextLabel.text = @"Features: \"YouTube-X/Adblock\", \"AutoPlay In Fullscreen\"";
-            }
-            if (indexPath.row == 4) {
-                cell.textLabel.text = @"Snoolie";
-                cell.detailTextLabel.text = @"Features: \"Enable Extra Video Speed\"";
-            }
-        }
     }
+    
+    NSDictionary *credit = self.credits[indexPath.row];
+    cell.textLabel.text = credit[@"name"];
+    cell.detailTextLabel.text = credit[@"role"];
+    
     return cell;
 }
 
-- (void)tableView:(UITableView *)theTableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    [theTableView deselectRowAtIndexPath:indexPath animated:YES];
-    if (indexPath.section == 0) {
-        if (indexPath.row == 0) {
-            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://github.com/LillieH001"] options:@{} completionHandler:nil];
-        }
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    NSDictionary *credit = self.credits[indexPath.row];
+    NSURL *url = [NSURL URLWithString:credit[@"url"]];
+    if (url) {
+        [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
     }
-    if (indexPath.section == 1) {
-        if (indexPath.row == 0) {
-            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://twitter.com/Kutarin_"] options:@{} completionHandler:nil];
-        }
-        if (indexPath.row == 1) {
-            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://github.com/Dayanch96"] options:@{} completionHandler:nil];
-        }
-        if (indexPath.row == 2) {
-            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://github.com/kirb"] options:@{} completionHandler:nil];
-        }
-        if (indexPath.row == 3) {
-            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://twitter.com/PoomSmart"] options:@{} completionHandler:nil];
-        }
-        if (indexPath.row == 4) {
-            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://github.com/0xilis"] options:@{} completionHandler:nil];
-        }
-    }
-}
-
-- (void)coloursView {
-    if (self.traitCollection.userInterfaceStyle == UIUserInterfaceStyleLight) {
-        self.view.backgroundColor = [UIColor colorWithRed:0.949 green:0.949 blue:0.969 alpha:1.0];
-        [self.navigationController.navigationBar setTitleTextAttributes:@{NSForegroundColorAttributeName:[UIColor blackColor]}];
-        self.navigationController.navigationBar.barStyle = UIBarStyleDefault;
-    }
-    else {
-        self.view.backgroundColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:1.0];
-        [self.navigationController.navigationBar setTitleTextAttributes:@{NSForegroundColorAttributeName:[UIColor whiteColor]}];
-        self.navigationController.navigationBar.barStyle = UIBarStyleBlack;
-    }
-}
-
-- (void)traitCollectionDidChange:(UITraitCollection *)previousTraitCollection {
-    [super traitCollectionDidChange:previousTraitCollection];
-    [self coloursView];
-    [self.tableView reloadData];
-}
-
-- (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-    self.view.layer.cornerRadius = 10.0;
-    self.view.layer.masksToBounds = YES;
-}
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    self.tableView.layer.borderWidth = 1.0;
-    self.tableView.layer.borderColor = [UIColor blackColor].CGColor;
-    self.view.layer.borderWidth = 1.0;
-    self.view.layer.borderColor = [UIColor blackColor].CGColor;
-    UITableView *tableView = self.tableView;
-    tableView.contentInset = UIEdgeInsetsMake(10.0, 0.0, 0.0, 0.0);
-    tableView.layer.maskedCorners = kCALayerMinXMinYCorner;
-    self.view.layer.cornerRadius = 10.0;
-    self.view.layer.masksToBounds = YES;
-    self.view.layer.maskedCorners = kCALayerMaxXMinYCorner | kCALayerMinXMinYCorner;
-}
-
-@end
-
-@implementation CreditsController (Privates)
-
-- (void)done {
-    [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
