1af2314ba22c5ecebbec2b1d6cf075ddc4202b14
aric3435 (INACTIVE) <78001398+arichornlover@users.noreply.github.com>
2024-10-10 20:54:15 -0500

    Update Localization.m

--- Controllers/Localization.m
+++ Controllers/Localization.m
@@ -4,12 +4,19 @@
 NSBundle *YouTubeRebornBundle() {
     static NSBundle *bundle = nil;
     static dispatch_once_t onceToken;
-	dispatch_once(&onceToken, ^{
+    dispatch_once(&onceToken, ^{
         NSString *tweakBundlePath = [[NSBundle mainBundle] pathForResource:@"YouTubeReborn" ofType:@"bundle"];
-        if (tweakBundlePath)
+        if (tweakBundlePath) {
             bundle = [NSBundle bundleWithPath:tweakBundlePath];
-        else
-            bundle = [NSBundle bundleWithPath:ROOT_PATH_NS("/Library/Application Support/YouTubeReborn.bundle")];
+        } else {
+            char resolvedPath[PATH_MAX];
+            const char *rootPath = libroot_dyn_jbrootpath("/Library/Application Support/YouTubeReborn.bundle", resolvedPath);
+            if (rootPath) {
+                bundle = [NSBundle bundleWithPath:[NSString stringWithUTF8String:rootPath]];
+            } else {
+                bundle = [NSBundle bundleWithPath:@"/Library/Application Support/YouTubeReborn.bundle"];
+            }
+        }
     });
     return bundle;
 }
